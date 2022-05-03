0f6620d9b0608ae1f43227288f5d0500bbafbc42
michele camera <michy.cam95@gmail.com>
2022-05-03 16:21:19 +0200

    modify

--- snack3/index.php
+++ snack3/index.php
@@ -4,7 +4,7 @@
 //Creare un array con 15 numeri casuali, 
 //tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta
 
-$array = array();
+$array = [];
 $min = 0;
 $max = 100;
 
